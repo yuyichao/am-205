543b0b7975df5de90e9f86a7768e569317843811
Yichao Yu <yyc1992@gmail.com>
2015-10-07 01:00:25 -0400

    [pset2] fsolve!

--- psets/pset2/bin_num.jl
+++ psets/pset2/bin_num.jl
@@ -15,10 +15,10 @@ Base.one(::Type{BinNum}) = BinNum(true)
 Base.zero(::BinNum) = BinNum(false)
 Base.one(::BinNum) = BinNum(true)
 
-*(x::BinNum, y::BinNum) = BinNum(x.v && y.v)
-+(x::BinNum, y::BinNum) = BinNum(x.v $ y.v)
-/(x::BinNum, y::BinNum) = BinNum(x.v ÷ y.v)
-\(x::BinNum, y::BinNum) = BinNum(y.v ÷ x.v)
+@inline *(x::BinNum, y::BinNum) = BinNum(x.v && y.v)
+@inline +(x::BinNum, y::BinNum) = BinNum(x.v $ y.v)
+@inline /(x::BinNum, y::BinNum) = BinNum(x.v ÷ y.v)
+@inline \(x::BinNum, y::BinNum) = BinNum(y.v ÷ x.v)
 
 # L = BinNum[1 0 0 0
 #            0 1 0 0
@@ -31,3 +31,27 @@ Base.one(::BinNum) = BinNum(true)
 #            0 0 0 1]
 
 # println(L * U)
+
+# Modify L, b and out in place
+function fsolve!(L::Matrix{BinNum}, b::Vector{BinNum}, out=similar(b))
+    len = length(b)
+    if size(L) != (len, len) || length(out) != len
+        throw(ArgumentError("Whatever...."))
+    end
+    @inbounds for i in 1:len
+        if !L[i, i].v
+            throw(DivideError())
+        end
+        if b[i].v
+            # SIMD for this doesn't currently work
+            # JuliaLang/julia#13104
+            @simd for j in (i + 1):len
+                b[j] += L[j, i]
+            end
+        end
+        @simd for j in (i + 1):len
+            L[j, i] = false
+        end
+    end
+    b
+end
