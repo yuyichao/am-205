60b743807894257d80dd32636397deb3c7ed630f
Yichao Yu <yyc1992@gmail.com>
2015-10-07 10:30:31 -0400

    [pset2] lu

--- psets/pset2/bin_num.jl
+++ psets/pset2/bin_num.jl
@@ -1,12 +1,12 @@
 #!/usr/bin/julia -f
 
-import Base: *, +, /, \
+import Base: *, +, /, \, -
 
 immutable BinNum
     v::Bool
 end
 
-Base.show(io::IO, n::BinNum) = Base.write(io, n.v ? "1b" : "0b")
+Base.show(io::IO, n::BinNum) = Base.write(io, n.v ? "1" : "0")
 
 Base.convert(::Type{BinNum}, v::Bool) = BinNum(v)
 Base.convert{T<:Integer}(::Type{BinNum}, v::T) = BinNum(v != 0)
@@ -14,9 +14,14 @@ Base.zero(::Type{BinNum}) = BinNum(false)
 Base.one(::Type{BinNum}) = BinNum(true)
 Base.zero(::BinNum) = BinNum(false)
 Base.one(::BinNum) = BinNum(true)
+Base.real(n::BinNum) = n
+Base.abs(n::BinNum) = n
+Base.isless(n1::BinNum, n2::BinNum) = n1.v < n2.v
+Base.inv(n::BinNum) = BinNum(true) / n
 
 @inline *(x::BinNum, y::BinNum) = BinNum(x.v && y.v)
 @inline +(x::BinNum, y::BinNum) = BinNum(x.v $ y.v)
+@inline -(x::BinNum, y::BinNum) = BinNum(x.v $ y.v)
 @inline /(x::BinNum, y::BinNum) = BinNum(x.v ÷ y.v)
 @inline \(x::BinNum, y::BinNum) = BinNum(y.v ÷ x.v)
 
@@ -78,4 +83,73 @@ function rsolve!(U::Matrix{BinNum}, b::Vector{BinNum}, out=similar(b))
         end
     end
     b
+end
+
+function Base.lu(A::Matrix{BinNum})
+    m, n = size(A)
+    L = eye(BinNum, m)
+    U = copy(A)
+    Plist = [1:m;]
+    l = min(m, n)
+    @inbounds for j in 1:(l - 1)
+        # Find a `1` in this column
+        k = -1
+        for i in j:m
+            if U[i, j].v
+                k = i
+                break
+            end
+        end
+        # Give up if there isn't one
+        k == -1 && continue
+        # Only do the copying if it is necessary
+        if k != j
+            # Update permutation list
+            Plist[j], Plist[k] = Plist[k], Plist[j]
+            # Update U
+            for i in j:n
+                U[j, i], U[k, i] = U[k, i], U[j, i]
+            end
+            # Update L
+            for i in 1:(j - 1)
+                L[j, i], L[k, i] = L[k, i], L[j, i]
+            end
+        end
+        # Tweak the algorithm so that the inner loop is along the stride-1
+        # direction
+        for k in (j + 1):n
+            if U[j, k].v
+                # SIMD probably doesn't work either...
+                @simd for i in (j + 1):m
+                    U[i, k] -= U[i, j]
+                end
+            end
+        end
+        @simd for i in (j + 1):m
+            L[i, j] = U[i, j]
+            U[i, j] = false
+        end
+    end
+    L, U, Plist
+end
+
+function lu_perm(A::Matrix{BinNum})
+    L, U, Plist = lu(A)
+    P = zeros(BinNum, m, m)
+    @inbounds for i in 1:m
+        P[i, Plist[i]] = true
+    end
+    L, U, P
+end
+
+# Debugging only =)
+function lu_perm_base(A::Matrix{BinNum})
+    # add the optional parameter so that we dispatch to the generic
+    # Base version
+    L, U, Plist = lu(A, Val{true})
+    P = zeros(BinNum, m, m)
+    @inbounds for i in 1:m
+        P[i, Plist[i]] = true
+    end
+    L, U, P
 end
