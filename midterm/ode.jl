c7f81de17105aa5a86fd5b43e7df6c9c57f6e55f
Yichao Yu <yyc1992@gmail.com>
2015-11-12 22:39:57 -0500

    Fix typos...

--- midterm/ode.jl
+++ midterm/ode.jl
@@ -17,8 +17,9 @@ function ode_rk4(f, t0, y0, h, cb)
         k3 = f(t + h2, y)
         y = muladd(h, k3, y_prev)
         k4 = f(t + h, y)
-        y = muladd(h6, k1, y_prev) + muladd(h3, k2, h3 * k3) + h6 * k4
+        y = muladd(h6, muladd(2, k2, k1) + muladd(2, k3, k4), y_prev)
         cb(y, i, t) || break
+        y_prev = y
     end
 end
 
@@ -52,9 +53,9 @@ immutable CubicSpline{T}
         # y1′:   x^3 - 2x^2 + x
         # y2′:   x^3 -  x^2
         a0 = y1
-        a1 = y1′ / δt
-        a2 = -3 * y1 + 3 * y2 - 2 * y1′ - y2′ / δt^2
-        a3 = 2 * y1 - 2 * y2 + y1′ + y2′ / δt^3
+        a1 = y1′
+        a2 = -3 * y1 + 3 * y2 - 2 * y1′ - y2′
+        a3 = 2 * y1 - 2 * y2 + y1′ + y2′
         new(a0, a1, a2, a3)
     end
 end
@@ -76,19 +77,19 @@ function find_ode_zero{T}(f, t0, y0::T, h)
     y′_prev = f(t_prev, y_prev)
     y′_last = f(t_last, y_last)
     @assert y_prev >= 0 && y_last < 0
-    sp = CubicSpline(y_prev, y_last, y_prev′, y_last′)
+    sp = CubicSpline(y_prev, y_last, y′_prev * h, y′_last * h)
     # Do bisect on relative time to avoid doing floating point math on ulp level
     ɛ = oftype(h, 1e-15) / h
     x_lower = zero(t_prev)
     x_upper = one(t_last)
-    x_mid = x_upper
+    x_mid = (x_upper + x_lower) / 2
     while x_upper - x_lower >= ɛ
-        x_mid = (x_upper + x_lower) / 2
         if sp(x_mid) >= 0
             x_lower = x_mid
         else
             x_upper = x_mid
         end
+        x_mid = (x_upper + x_lower) / 2
     end
-    x_mid * h + t_last
+    muladd(x_mid, h, t_prev)
 end
