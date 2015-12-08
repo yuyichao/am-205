6142561b2c061ead9eba885fb06fe4e65c9fa7fa
Yichao Yu <yyc1992@gmail.com>
2015-12-08 09:01:44 -0500

    [final] mesh first (kind of) working version

--- final/MeshGen/src/meshes.jl
+++ final/MeshGen/src/meshes.jl
@@ -61,6 +61,17 @@ tile and the coordinate of the intersection
 """
 function check_crossing end
 
+"""
+    check_crossing{N,V}(model::AbstractModel{N,V}, orig_points::NTuple{N,V},
+                        point2::V, tileset::TileSet{N,V})
+        -> Vector{Int}
+
+Check if the lines from `orig_point` to `point2` intersect with any tile in
+`tileset`. Return an empty `Vector` if not. Otherwise, return the indecis of
+the intersecting tiles.
+"""
+function check_crossing end
+
 typealias Abstract1D{V} AbstractModel{1,V}
 
 function init_mesh{V}(model::Abstract1D{V}, mesh, frontier, dirs, sections)
@@ -82,6 +93,7 @@ function init_mesh{V}(model::Abstract1D{V}, mesh, frontier, dirs, sections)
         dirs[fidx] = -dir
         sections[fidx] = next_sec2
     end
+    nothing
 end
 
 function mesh{V}(model::Abstract1D{V})
@@ -133,7 +145,7 @@ function mesh{V}(model::Abstract1D{V})
                 @assert next_sec2 == next_sec
                 diff_vec = next_p - p
                 step_len = abs(diff_vec)
-                @assert step_len > 0
+                step_len <= step / 10 && continue
                 if step_len < step * √(2)
                     # if the boundary is closed enough, simply use that point
                     push!(mesh, (p, next_p))
@@ -216,5 +228,476 @@ function mesh{V}(model::Abstract1D{V})
 end
 
 typealias Abstract2D{T} AbstractModel{2,T}
+
+immutable WorkSet2D{V}
+    edges::TileSet{2,V} # All edges
+    fr::Set{Int} # Current frontier
+    dirs::Dict{Int,V} # Directions
+    psect::Dict{Int,Set{Int}} # Sections of points
+    esect::Dict{Int,Int} # Sections of points
+    WorkSet2D(pset) = new(TileSet{2}(pset), Set{Int}(), Dict{Int,V}(),
+                          Dict{Int,Set{Int}}(), Dict{NTuple{2,Int},Int}())
+end
+
+function get_psects!{V}(ws::WorkSet2D{V}, pidx::Int)
+    if pidx in keys(ws.psect)
+        ws.psect[pidx]
+    else
+        ws.psect[pidx] = Set{Int}()
+    end
+end
+
+function add_edge_section!{V}(ws::WorkSet2D{V}, eidx::Int, section::Int)
+    ws.esect[eidx] = section
+end
+
+function add_section!{V}(ws::WorkSet2D{V}, pidx::Int, section::Int)
+    sects = get_psects!(ws, pidx)
+    push!(sects, section)
+    nothing
+end
+
+add_section!{V}(ws::WorkSet2D{V}, p::V, section::Int) =
+    add_section!(ws, ws.edges.pts[p], section)
+
+function get_section{V}(ws::WorkSet2D{V}, pidx1::Int, pidx2::Int)
+    eidx = ws.edges[(pidx1, pidx2)]
+    sects1 = get_psects!(ws, pidx1)
+    sects2 = get_psects!(ws, pidx2)
+    if eidx in keys(ws.esect)
+        section = ws.esect[eidx]
+        sects1 = delete!(copy(sects1), section)
+        sects2 = delete!(copy(sects2), section)
+        sec1 = isempty(sects1) ? section : first(sects1)
+        sec2 = isempty(sects2) ? section : first(sects2)
+        return sec1, sec2, section
+    end
+    if isempty(sects1)
+        isempty(sects2) && return (-1, -1, -1)
+        sec = first(sects2)
+        return (-1, sec, sec)
+    elseif isempty(sects2)
+        sec = first(sects1)
+        return (sec, -1, sec)
+    end
+    for sec in sects1
+        sec in sects2 && return (sec, sec, sec)
+    end
+    sec1 = first(sects1)
+    return (sec1, first(sects2), sec1)
+end
+get_section{V}(ws::WorkSet2D{V}, p1::V, p2::V) =
+    get_section(ws, ws.edges.pts[p1], ws.edges.pts[p2])
+
+function Base.push!{V}(ws::WorkSet2D{V}, pidxs::NTuple{2,Int}, dir::V)
+    eidx = push!(ws.edges, pidxs)
+    push!(ws.fr, eidx)
+    ws.dirs[eidx] = dir
+    eidx
+end
+Base.push!{V}(ws::WorkSet2D{V}, ps::NTuple{2,V}, dir::V) =
+    push!(ws, ws.edges.pts[ps], dir)
+
+Base.isempty(ws::WorkSet2D) = isempty(ws.fr)
+
+Base.start(ws::WorkSet2D) = start(ws.fr)
+function Base.next(ws::WorkSet2D, idx)
+    eidx, idx = next(ws.fr, idx)
+    pidxs = ws.edges[eidx]
+    dir = ws.dirs[eidx]
+    (eidx, ws.edges.pts[pidxs], dir), idx
+end
+Base.done(ws::WorkSet2D, idx) = done(ws.fr, idx)
+
+function Base.delete!(ws::WorkSet2D, eidx::Int)
+    delete!(ws.fr, eidx)
+    delete!(ws.dirs, eidx)
+end
+function Base.getindex(ws::WorkSet2D, eidx::Int)
+    pidxs = ws.edges[eidx]
+    dir = ws.dirs[eidx]
+    ws.edges.pts[pidxs], dir
+end
+
+function init_mesh{V}(model::Abstract2D{V}, mesh, ws)
+    # Get initial point
+    p, section, (dir1, dir2) = get_init(model)
+    step = get_step_size(model, p, section)
+    next_p1, next_sec1 = get_next_point(model, p, step * dir1, section, true)
+    next_p2, next_sec2 = get_next_point(model, p, step * dir2, section, true)
+    push!(mesh, (p, next_p1, next_p2))
+    dir3 = dir1 + dir2
+    dir3 /= -abs(dir3)
+    dir1, dir2 = calc_directions(next_p1, next_p2, p)
+    eidx1 = push!(ws, (next_p1, p), dir1)
+    eidx2 = push!(ws, (next_p2, p), dir2)
+    eidx12 = push!(ws, (next_p1, next_p2), dir3)
+    add_section!(ws, next_p1, next_sec1)
+    add_section!(ws, next_p2, next_sec2)
+    add_section!(ws, p, section)
+    add_section!(ws, next_p1, section)
+    add_section!(ws, next_p2, section)
+    add_edge_section!(ws, eidx1, section)
+    add_edge_section!(ws, eidx2, section)
+    add_edge_section!(ws, eidx12, section)
+    nothing
+end
+
+function calc_directions(old_p1, old_p2, new_p)
+    edge1 = new_p - old_p1
+    edge2 = new_p - old_p2
+    ledge1 = abs(edge1)
+    ledge2 = abs(edge2)
+    @assert ledge1 > 0 && ledge2 > 0
+    edge1 /= ledge1
+    edge2 /= ledge2
+
+    proj = edge1 * edge2
+
+    dir1 = edge2 - edge1 * proj
+    dir2 = edge1 - edge2 * proj
+    ldir1 = abs(dir1)
+    ldir2 = abs(dir2)
+    @assert ldir1 > 0 && ldir2 > 0
+    dir1 / ldir1, dir2 / ldir2
+end
+
+function normalized_dot(v1, v2)
+    v1 /= abs(v1)
+    v2 /= abs(v2)
+    v1 * v2
+end
+
+# function recheck_crossing(model, p1, p2, new_p, fr, section)
+#     pset = fr.edges.pts
+#     pmid = (p1 + p2) / 2
+#     iscross = check_crossing(model, (p1, p2), new_p, fr.edges)
+#     for cr_tidx in iscross
+#         cr_pidx1, cr_pidx2 = fr.edges[cr_tidx]
+#         cr_p1 = pset[cr_pidx1]
+#         cr_p2 = pset[cr_pidx2]
+#         (cr_p1 == new_p || cr_p2 == new_p) && continue
+#         # There's another edge in between, shrink the vector and try again.
+#         step_scale = oftype(abs(pmid), 0.9)
+#         @label retry
+#         next_p, next_sec =
+#             get_next_point(model, pmid, (new_p - pmid) * step_scale,
+#                            section, true)
+#         iscross = check_crossing(model, (p1, p2), next_p, fr.edges)
+#         if isempty(iscross)
+#             step_scale = (step_scale + 1) / 2
+#             @goto retry
+#         end
+#         return iscross
+#     end
+#     Int[]
+# end
+
+function orthorg_vec(v1, v2)
+    n1 = v1 / abs(v1)
+    v2 - (n1 * v2) * n1
+end
+
+function distance_to_segment(v1, v2)
+    # p0 -> p1: v1
+    # p0 -> p2: v2
+    # p1 -> p2: v2 - v1 = -v12
+    v12 = v1 - v2
+
+    # check if angle 1 is larger than 90deg
+    if v1 * v12 <= 0
+        return abs(v1)
+    end
+    # check if angle 2 is larger than 90deg
+    if v2 * v12 >= 0
+        return abs(v2)
+    end
+    # use the distance
+    abs(orthorg_vec(v12, v1))
+end
+
+function check_neighbers(model, ws, pset, mesh, p1, p2, dir, section)
+    for (eidx, etile) in ws.edges.tiles
+        ep1, ep2 = pset[etile]
+        ((ep1 == p1 && ep2 == p2) || (ep1 == p2 && ep2 == p1)) && continue
+        if ep1 == p1
+            p_com = p1
+            p_old = p2
+            p_new = ep2
+        elseif ep2 == p1
+            p_com = p1
+            p_old = p2
+            p_new = ep1
+        elseif ep1 == p2
+            p_com = p2
+            p_old = p1
+            p_new = ep2
+        elseif ep2 == p2
+            p_com = p2
+            p_old = p1
+            p_new = ep1
+        else
+            continue
+        end
+        (p_new - p_com) * dir <= 0 && continue
+        normalized_dot(p_new - p_com, p_old - p_com) < 0.5 && continue
+        isempty(check_crossing(model, (p_old, p_com), p_new,
+                               ws.edges)) || continue
+        push!(mesh, (p_old, p_new, p_com))
+        # delete the other edge
+        delete!(ws, eidx)
+        try
+            # Check if the thrid edge also exist
+            eidx2 = ws.edges[(p_old, p_new)]
+            delete!(ws, eidx2)
+            return true
+        end
+        dir_pold, dir_pnew = calc_directions(p_com, p_old, p_new)
+        eidx_new = push!(ws, (p_old, p_new), dir_pnew)
+        add_section!(ws, p_new, section)
+        add_edge_section!(ws, eidx_new, section)
+        return true
+    end
+    return false
+end
+
+function check_in_circle(model, ws, pset, mesh, p1, p2, step, dir, section)
+    pmid = (p1 + p2) / 2
+    dir = orthorg_vec(p1 - p2, dir)
+    dir = dir / abs(dir)
+    po = pmid + step * dir * oftype(step, 0.8)
+    ro = abs(po - p1)
+    remset = Set{Int}()
+    for (eidx, etile) in ws.edges.tiles
+        ep1, ep2 = pset[etile]
+        ((ep1 == p1 && ep2 == p2) || (ep1 == p2 && ep2 == p1)) && continue
+        # Check if the edge is in the circle
+        distance_to_segment(ep1 - po, ep2 - po) >= ro && continue
+        ep1_valid = (ep1 - pmid) * dir > 0 && !(ep1 == p1 || ep1 == p2)
+        ep2_valid = (ep2 - pmid) * dir > 0 && !(ep2 == p1 || ep2 == p2)
+        cross1 = if ep1_valid
+            check_crossing(model, (p1, p2), ep1, ws.edges)
+        else
+            Int[]
+        end
+        cross2 = if ep2_valid
+            check_crossing(model, (p1, p2), ep2, ws.edges)
+        else
+            Int[]
+        end
+        ep1_valid &= isempty(cross1)
+        ep2_valid &= isempty(cross2)
+        if !(ep1_valid || ep2_valid)
+            union!(remset, cross1)
+            union!(remset, cross2)
+            continue
+        end
+        p_new = ep1_valid ? ep1 : ep2
+
+        push!(mesh, (p1, p2, p_new))
+        # delete other edges
+        eg1_exists = false
+        eg2_exists = false
+        try
+            eidx1 = ws.edges[(p1, p_new)]
+            delete!(ws, eidx1)
+            eg1_exists = true
+        end
+        try
+            eidx2 = ws.edges[(p2, p_new)]
+            delete!(ws, eidx2)
+            eg2_exists = true
+        end
+        dir_p1, dir_p2 = calc_directions(p1, p2, p_new)
+        if !eg1_exists
+            eidx_new1 = push!(ws, (p1, p_new), dir_p1)
+            add_edge_section!(ws, eidx_new1, section)
+        end
+        if !eg2_exists
+            eidx_new2 = push!(ws, (p2, p_new), dir_p2)
+            add_edge_section!(ws, eidx_new2, section)
+        end
+        add_section!(ws, p_new, section)
+        return true
+    end
+    isempty(remset) && return false
+    threshold = oftype(step, Inf)
+    remset2 = Set{Int}()
+    while !isempty(remset)
+        new_thresh = oftype(step, -Inf)
+        for eidx in remset
+            epidx1, epidx2 = ws.edges[eidx]
+            ep1 = pset[epidx1]
+            ep2 = pset[epidx2]
+            ep1_valid = (ep1 - pmid) * dir > 0 && !(ep1 == p1 || ep1 == p2)
+            ep2_valid = (ep2 - pmid) * dir > 0 && !(ep2 == p1 || ep2 == p2)
+            dot1 = normalized_dot(ep1 - p1, ep1 - p2)
+            dot2 = normalized_dot(ep2 - p1, ep2 - p2)
+            ep1_valid &= dot1 < threshold
+            ep2_valid &= dot2 < threshold
+
+            cross1 = if ep1_valid
+                check_crossing(model, (p1, p2), ep1, ws.edges)
+            else
+                Int[]
+            end
+            cross2 = if ep2_valid
+                check_crossing(model, (p1, p2), ep2, ws.edges)
+            else
+                Int[]
+            end
+            if ep1_valid
+                new_thresh = max(dot1, new_thresh)
+            end
+            if ep2_valid
+                new_thresh = max(dot2, new_thresh)
+            end
+            ep1_valid &= isempty(cross1)
+            ep2_valid &= isempty(cross2)
+            if !(ep1_valid || ep2_valid)
+                union!(remset2, cross1)
+                union!(remset2, cross2)
+                continue
+            end
+            p_new = ep1_valid ? ep1 : ep2
+
+            push!(mesh, (p1, p2, p_new))
+            # delete other edges
+            eg1_exists = false
+            eg2_exists = false
+            try
+                eidx1 = ws.edges[(p1, p_new)]
+                delete!(ws, eidx1)
+                eg1_exists = true
+            end
+            try
+                eidx2 = ws.edges[(p2, p_new)]
+                delete!(ws, eidx2)
+                eg2_exists = true
+            end
+            dir_p1, dir_p2 = calc_directions(p1, p2, p_new)
+            if !eg1_exists
+                eidx_new1 = push!(ws, (p1, p_new), dir_p1)
+                add_edge_section!(ws, eidx_new1, section)
+            end
+            if !eg2_exists
+                eidx_new2 = push!(ws, (p2, p_new), dir_p2)
+                add_edge_section!(ws, eidx_new2, section)
+            end
+            add_section!(ws, p_new, section)
+            return true
+        end
+        remset, remset2 = remset2, remset
+        empty!(remset2)
+        threshold = new_thresh
+    end
+    @assert false
+    return false
+end
+
+function handle_next_boundary(model, ws, pset, mesh, p1, p2, p_new, step,
+                              sec_p1, sec_p2, section, next_sec)
+    pmid = (p1 + p2) / 2
+    diff_vec = p_new - pmid
+    step_len = abs(diff_vec)
+    @assert step_len > 0
+    if step_len < step * √(2)
+        # if the boundary is closed enough, simply use that point
+        push!(mesh, (p1, p2, p_new))
+        dir_p1, dir_p2 = calc_directions(p1, p2, p_new)
+        sec_new1 = sec_p1 == next_sec ? next_sec : section
+        sec_new2 = sec_p2 == next_sec ? next_sec : section
+        eidx_new1 = push!(ws, (p_new, p1), dir_p1)
+        eidx_new2 = push!(ws, (p_new, p2), dir_p2)
+        add_section!(ws, p_new, next_sec)
+        add_edge_section!(ws, eidx_new1, sec_new1)
+        add_edge_section!(ws, eidx_new2, sec_new2)
+    else
+        p_new, next_sec = get_next_point(model, pmid,
+                                         (p_new - pmid) / 2,
+                                         section, true)
+        @assert next_sec == section
+        push!(mesh, (p1, p2, p_new))
+        dir_p1, dir_p2 = calc_directions(p1, p2, p_new)
+        eidx_new1 = push!(ws, (p_new, p1), dir_p1)
+        eidx_new2 = push!(ws, (p_new, p2), dir_p2)
+        add_section!(ws, p_new, section)
+        add_edge_section!(ws, eidx_new1, section)
+        add_edge_section!(ws, eidx_new2, section)
+    end
+end
+
+function handle_next_empty(model, ws, pset, mesh, p1, p2, p_new, section)
+    pmid = (p1 + p2) / 2
+    p_new, next_sec = get_next_point(model, pmid, (p_new - pmid) / 2,
+                                     section, true)
+    @assert next_sec == section
+    push!(mesh, (p1, p2, p_new))
+    dir_p1, dir_p2 = calc_directions(p1, p2, p_new)
+    eidx_new1 = push!(ws, (p_new, p1), dir_p1)
+    eidx_new2 = push!(ws, (p_new, p2), dir_p2)
+    add_section!(ws, p_new, section)
+    add_edge_section!(ws, eidx_new1, section)
+    add_edge_section!(ws, eidx_new2, section)
+end
+
+function mesh{V}(model::Abstract2D{V})
+    pset = PointSet{V}()
+    mesh = TileSet{3}(pset)
+    ws = WorkSet2D{V}(pset)
+    init_mesh(model, mesh, ws)
+
+    counter = 0
+    while !isempty(ws)
+        (eidx, (p1, p2), dir) = first(ws)
+        (sec_p1, sec_p2, section) = get_section(ws, p1, p2)
+        delete!(ws, eidx)
+        counter += 1
+        # counter >= 4000 && break
+        section == -1 && continue
+        step1 = get_step_size(model, p1, section)
+        step2 = get_step_size(model, p2, section)
+        @assert step1 > 0
+        @assert step2 > 0
+        dist_p12 = abs(p1 - p2)
+        step = (step1 + step2) / 2
+        if step < dist_p12 / 3
+            step = oftype(step, dist_p12 / 3)
+        end
+
+        # 1. First check if the neighbering edges are good candidates, i.e.
+        #
+        #     1. on the correct side of the line
+        #     2. small enough angle
+        #     3. connecting them do not cross with other lines
+        check_neighbers(model, ws, pset, mesh, p1, p2, dir, section) && continue
+
+        # 2. If none of the neighbering edges are good, check if there's any
+        #     unterminated edges nearby. If yes, find a point among them.
+        check_in_circle(model, ws, pset, mesh, p1, p2, step,
+                        dir, section) && continue
+
+        # 3. Now we know there isn't any existing points nearby, we can
+        #     treat it as empty space. We first need to check if we will hit
+        #     a boundary soon. Go twice as far (or some distance we know is
+        #     not enough to hit any other existing edges)
+        pmid = (p1 + p2) / 2
+        p_new, sec_new = get_next_point(model, pmid,
+                                        step * oftype(step, √(2)) * dir,
+                                        section, true)
+        #
+        #     1. If we hit an edge, truncate our step and use the point on the
+        #         edge.
+        if sec_new != section
+            handle_next_boundary(model, ws, pset, mesh, p1, p2, p_new, step,
+                                 sec_p1, sec_p2, section, sec_new)
+            continue
+        end
+        #     2. Otherwise, use the normal size
+        handle_next_empty(model, ws, pset, mesh, p1, p2, p_new, section)
+    end
+
+    mesh, ws.edges
+end
 
 end
--- final/MeshGen/test/meshes.jl
+++ final/MeshGen/test/meshes.jl
@@ -70,4 +70,128 @@ ps = mesh(CircleModel(0.1))
 # grid()
 # show()
 
+immutable CircleModel2D{T} <: Meshes.Abstract2D{Vec{2,T}}
+end
+
+function Meshes.get_section{T}(::CircleModel2D{T}, p::Vec{2,T})
+    abs(p) >= 1 ? -1 : 0
+end
+function Meshes.get_step_size{T}(::CircleModel2D{T}, p::Vec{2,T}, section::Int)
+    T(0.05) / T(abs(p) + 0.3)
+end
+
+function Meshes.get_init{T}(::CircleModel2D{T})
+    Vec{2,T}(0, 0), 0, (Vec{2,T}(1, 0), Vec{2,T}(0.5, √(3) / 2))
+end
+
+function Meshes.get_next_point{T}(::CircleModel2D{T}, p::Vec{2,T},
+                                  step::Vec{2,T}, section::Int, clip::Bool)
+    next_p = p + step
+    section = abs(next_p) >= 1 ? -1 : 0
+    if section == -1 && clip
+        next_p /= abs(next_p)
+    end
+    next_p, section
+end
+
+function same_side_of(lp1::Vec{2}, lp2::Vec{2}, p1::Vec{2}, p2::Vec{2})
+    # http://stackoverflow.com/a/7069702/904262
+    lv = lp2 - lp1
+    p11 = p1 - lp1
+    p21 = p2 - lp1
+    # which side is p1 on calculate lv × p11
+    side1 = lv.r[1] * p11.r[2] - lv.r[2] * p11.r[1]
+    # which side is p2 on calculate lv × p21
+    side2 = lv.r[1] * p21.r[2] - lv.r[2] * p21.r[1]
+    side1 * side2 > 0
+end
+
+function line_crosses(l1p1, l1p2, l2p1, l2p2)
+    # http://stackoverflow.com/a/7069702/904262
+    same_side_of(l1p1, l1p2, l2p1, l2p2) && return false
+    same_side_of(l2p1, l2p2, l1p1, l1p2) && return false
+    return true
+end
+
+function point_in_angle(common, p1, p2, ptest)
+    same_side_of(common, ptest, p1, p2) && return false
+    v1 = p1 - common
+    v2 = p2 - common
+    vtest = ptest - common
+    v1 /= abs(v1)
+    v2 /= abs(v2)
+    vtest /= abs(vtest)
+    v1 * vtest <= 0 && v2 * vtest <= 0 && return false
+    acos(v1 * vtest) + acos(v2 * vtest) <= π
+end
+
+function Meshes.check_crossing{T}(::CircleModel2D{T}, ps::NTuple{2,Vec{2,T}},
+                                  p2::Vec{2,T}, tileset::TileSet{2,Vec{2,T}})
+    crossing = Int[]
+    pset = tileset.pts
+    pidx1 = pset[ps[1]]
+    pidx2 = pset[ps[2]]
+    for (fidx, ftile) in tileset.tiles
+        ftidx1, ftidx2 = ftile
+        if ((ftidx1 == pidx1 && ftidx2 == pidx2) ||
+            (ftidx1 == pidx2 && ftidx2 == pidx1))
+            continue
+        end
+        ftp1, ftp2 = pset[ftile]
+        if ((ftp1 == ps[1] && ftp2 == p2) || (ftp1 == p2 && ftp2 == ps[1]))
+            continue
+        end
+        if ((ftp1 == ps[2] && ftp2 == p2) || (ftp1 == p2 && ftp2 == ps[2]))
+            continue
+        end
+        if pidx1 == ftidx1
+            point_in_angle(ps[1], p2, ps[2], ftp2) && push!(crossing, fidx)
+            continue
+        elseif pidx1 == ftidx2
+            point_in_angle(ps[1], p2, ps[2], ftp1) && push!(crossing, fidx)
+            continue
+        elseif pidx2 == ftidx1
+            point_in_angle(ps[2], p2, ps[1], ftp2) && push!(crossing, fidx)
+            continue
+        elseif pidx2 == ftidx2
+            point_in_angle(ps[2], p2, ps[1], ftp1) && push!(crossing, fidx)
+            continue
+        elseif ftp1 == p2
+            point_in_angle(p2, ps[2], ps[1], ftp2) && push!(crossing, fidx)
+            continue
+        elseif ftp2 == p2
+            point_in_angle(p2, ps[2], ps[1], ftp1) && push!(crossing, fidx)
+            continue
+        end
+        if (line_crosses(ftp1, ftp2, ps[1], p2) ||
+            line_crosses(ftp1, ftp2, ps[2], p2))
+            push!(crossing, fidx)
+        end
+    end
+    crossing
+end
+
+ps2d, frontier = mesh(CircleModel2D{Float64}())
+
+using PyPlot
+pset2 = ps2d.pts
+figure()
+for (idx, tile) in ps2d.tiles
+    r1, r2, r3 = pset2[tile]
+    plot([r1.r[1], r2.r[1], r3.r[1], r1.r[1]],
+         [r1.r[2], r2.r[2], r3.r[2], r1.r[2]], "o-")
+end
+grid()
+figure()
+for (idx, tile) in frontier.tiles
+    r1, r2 = pset2[tile]
+    plot([r1.r[1], r2.r[1]], [r1.r[2], r2.r[2]], "bo-")
+    # if idx in ignored
+    #     plot([r1.r[1], r2.r[1]], [r1.r[2], r2.r[2]], "go-")
+    # else
+    # end
+end
+grid()
+show()
+
 end
